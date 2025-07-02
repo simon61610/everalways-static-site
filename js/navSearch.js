c19e0aead471dd85ae7f1844f594a3954afc76eb
simon61610 <simon410379007@gmail.com>
2025-07-02 08:45:39 +0800

    新增搜尋欄的清空功能

--- js/navSearch.js
+++ js/navSearch.js
@@ -5,9 +5,19 @@ $(document).ready(function(){
             // slide後讓input自動focus
             $(this).find('input').focus()
         })
+
+        // 點擊後呼叫清空功能
+        clearInputValue()
     })
 
+    // 點叉叉後關閉搜尋欄
     $('#topSearch i:last-child').click(function(){
         $('.top-search').slideUp(500)
     })
+
+    // 功能: 清空搜尋欄的文字
+    function clearInputValue(){
+        let searchInput = $('#topSearch input')
+        searchInput.val('')
+    }
 })
