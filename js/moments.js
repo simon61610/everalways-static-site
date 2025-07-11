51afa2504b09027ac556336d44868cd79a13588c
simon61610 <simon410379007@gmail.com>
2025-07-11 16:55:03 +0800

    moments 切分頁與篩選功能完成

--- js/moments.js
+++ js/moments.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
+    // Tabs 選取後變色
+    $('#categoryTabs li:first').addClass('active')
 
-    
+    $('#categoryTabs li').click(function(){
+        $(this).addClass('active')
+        $('#categoryTabs li').not(this).removeClass('active')
+    })
+
+    // 變數設定
+    items = $('.gallery-item') // 複數是陣列，可以用陣列方法
+    itmesWestern = $('div[data-moments="western"]')
+    itmesChinese = $('div[data-moments="chinese"]')
+    itmesOutdoor = $('div[data-moments="outdoor"]')
+    itmesProposal = $('div[data-moments="proposal"]')
+    itmesBabyparty = $('div[data-moments="babyparty"]')
+
 
     const ITEMSPERPAGE = 9 // 每頁 9 筆
-    let items = $('.gallery-item') // 複數是陣列，可以用陣列方法
 
     // 先顯示第一頁
     showPage(0)
@@ -20,10 +33,19 @@ $(document).ready(function(){
         items.slice(start, end).fadeIn(1000)
     }
 
+        function showPageClick(page, type){
+        let start = page * ITEMSPERPAGE
+        let end = (page + 1 ) * ITEMSPERPAGE
+        
+        items.hide() // 不顯示
+        $(`div[data-moments="${type}"]`).slice(start, end).fadeIn(1000)
+    }
+
     // 動態新增圓圈圈
     for(let i = 0;i < Math.ceil(items.length / ITEMSPERPAGE) ;i++){
         $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
     }
+
     $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
 
     $('.dot').click(function(){
@@ -34,5 +56,175 @@ $(document).ready(function(){
         $(this).addClass('active')
         $('.dot').not(this).removeClass('active')
     })
-    
+
+    // 案例篩選
+    $('#allCases').click(function(){
+        items.show()
+        // alert(items.length)
+        showPage(0)
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(items.length / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPage($(this).index())
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
+    $('#western').click(function(){
+        items.each(function(){
+            if($(this).attr('data-moments') === 'western'){
+               $(this).show() 
+            }else{
+                $(this).hide()
+            }
+        })
+
+        countCaseShowd = itmesWestern.length;
+        // alert(countCaseShowd)
+        showPageClick(0, 'western')
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(countCaseShowd / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPageClick($(this).index(), 'western')
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
+    $('#chinese').click(function(){
+        items.each(function(){
+            if($(this).attr('data-moments') === 'chinese'){
+               $(this).show() 
+            }else{
+                $(this).hide()
+            }
+        })
+
+        countCaseShowd = itmesChinese.length;
+        // alert(countCaseShowd)
+        showPageClick(0, 'chinese')
+
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(countCaseShowd / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPageClick($(this).index(), 'chinese')
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
+    $('#outdoor').click(function(){
+        items.each(function(){
+            if($(this).attr('data-moments') === 'outdoor'){
+               $(this).show() 
+            }else{
+                $(this).hide()
+            }
+        })
+
+        countCaseShowd = itmesOutdoor.length;
+        // alert(countCaseShowd)
+        showPageClick(0, 'outdoor')
+
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(countCaseShowd / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPageClick($(this).index(), 'outdoor')
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
+    $('#proposal').click(function(){
+        items.each(function(){
+            if($(this).attr('data-moments') === 'proposal'){
+               $(this).show() 
+            }else{
+                $(this).hide()
+            }
+        })
+
+        countCaseShowd = itmesProposal.length;
+        // alert(countCaseShowd)
+        showPageClick(0, 'proposal')
+
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(countCaseShowd / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPageClick($(this).index(), 'proposal')
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
+    $('#babyparty').click(function(){
+        items.each(function(){
+            if($(this).attr('data-moments') === 'babyparty'){
+               $(this).show() 
+            }else{
+                $(this).hide()
+            }
+        })
+
+        countCaseShowd = itmesBabyparty.length;
+        // alert(countCaseShowd)
+        showPageClick(0, 'babyparty')
+
+
+        $('.dot').remove()
+        for(let i = 0;i < Math.ceil(countCaseShowd / ITEMSPERPAGE) ;i++){
+            $('#dotButton').append(`<li class="dot"><i class="fa-solid fa-circle"></i></li>`)
+        }
+
+        $('#dotButton li:first').addClass('active') // 第一顆按鈕先變色
+
+        $('.dot').click(function(){
+            // 顯示第 index 頁
+            showPageClick($(this).index(), 'babyparty')
+
+            // 圓圈圈變色
+            $(this).addClass('active')
+            $('.dot').not(this).removeClass('active')
+        })
+    })
 })
